1089209e5cec335eafd918ffcaa343acfb578a10
Yash Kaskar <yash.kaskar99@gmail.com>
2022-10-16 11:34:48 +0530

    Adding Select events

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const p2Display = document.querySelector('#p2Display');
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector('#resetButton');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 9;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', () => {    
@@ -30,10 +31,17 @@ p2Button.addEventListener('click', () => {
     }
 })
 
-resetButton.addEventListener('click', () => {   
+winningScoreSelect.addEventListener('change', function () {   
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset()  {   
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-})
+}
